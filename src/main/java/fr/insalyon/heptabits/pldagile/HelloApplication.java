cd52cdfc5839f8b15e9c86467ddee861728d0e1f
chloé <chloechabal172@gmail.com>
2023-11-21 12:14:05 +0100

    premier test de chargement XML

--- src/main/java/fr/insalyon/heptabits/pldagile/HelloApplication.java
+++ src/main/java/fr/insalyon/heptabits/pldagile/HelloApplication.java
@@ -4,7 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -15,7 +21,38 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        File file = new File("src/main/resources/fr/insalyon/heptabits/pldagile/ExamplesMap/smallMap.xml");
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //an instance of builder to parse the specified xml file
+        DocumentBuilder db;
+
+        {
+            try {
+                db = dbf.newDocumentBuilder();
+            } catch (ParserConfigurationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Document doc;
+
+        {
+            try {
+                doc = db.parse(file);
+            } catch (SAXException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        doc.getDocumentElement().normalize();
+        System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
     }
+
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module fr.insalyon.heptabits.pldagile {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.xml;
 
     opens fr.insalyon.heptabits.pldagile to javafx.fxml;
     exports fr.insalyon.heptabits.pldagile;
