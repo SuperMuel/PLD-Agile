065d19477c9629e5aa60e4f3dac906f69fa4fdb8
supermuel <supermuel@supermuel.fr>
2023-11-25 18:08:08 +0100

    Add length attribute to segment

--- src/main/java/fr/insalyon/heptabits/pldagile/model/Segment.java
+++ src/main/java/fr/insalyon/heptabits/pldagile/model/Segment.java
@@ -2,17 +2,17 @@ package fr.insalyon.heptabits.pldagile.model;
 
 public class Segment extends BaseEntity {
 
-    // has a origin and destination intersection ids, and a name
     private final long originId;
     private final long destinationId;
     private final String name;
+    private final double length;
 
-
-    public Segment(long id, long originId, long destinationId, String name) {
+    public Segment(long id, long originId, long destinationId, String name, double length) {
         super(id);
         this.originId = originId;
         this.destinationId = destinationId;
         this.name = name;
+        this.length = length;
     }
 
     public long getOriginId() {
@@ -38,4 +38,7 @@ public class Segment extends BaseEntity {
     }
 
 
+    public double getLength() {
+        return length;
+    }
 }
--- src/test/java/fr/insalyon/heptabits/pldagile/model/SegmentTest.java
+++ src/test/java/fr/insalyon/heptabits/pldagile/model/SegmentTest.java
@@ -15,7 +15,8 @@ class SegmentTest {
     void setUp() {
         origin = new Intersection(0, 0, 1);
         destination = new Intersection(1, 2, 3);
-        segment = new Segment(0, origin.getId(), destination.getId(), "Rue 1");
+
+        segment = new Segment(0, origin.getId(), destination.getId(), "Rue 1", 12.1);
     }
 
     @AfterEach
@@ -35,5 +36,10 @@ class SegmentTest {
     @Test
     void getName() {
         assertEquals("Rue 1", segment.getName());
+    }
+
+    @Test
+    void getLength() {
+        assertEquals(12.1, segment.getLength());
     }
 }
